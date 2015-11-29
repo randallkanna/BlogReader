e51b2e343e60c8a7395ebff2c7e22bf123b01b37
randallkanna <randallkanna@gmail.com>
2015-11-28 16:46:17 -0800

    initial commit

---
+++ BlogReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReader
+//
+//  Created by Randall Kanna on 11/28/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
